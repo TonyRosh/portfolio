2c5ea214cfeb9dbe7d698776bf4b43cdf1ec07f5
“Tony <adrprogramming@gmail.com>
2021-09-20 12:32:38 -0600

    fixed no used vars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Home from './components/Home'
-import About from './components/About'
 import Projects from './components/Projects'
 import Resume from './components/Resume'
 
