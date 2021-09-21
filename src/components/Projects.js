fc46ccb11628d1ef65881c4b3c15fddf1014d855
“Tony <adrprogramming@gmail.com>
2021-09-21 15:52:45 -0600

    i think i am finished

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -28,24 +28,24 @@ init();
         </div>
         <div class="slider">
 	<section>
-		<h2 className='projects-title'><a href="http://www.roscioli.dev" target="_blank" rel="noreferrer">My THREE.js Universe</a></h2>
-        <p>An amusing and intereactive way to get a glimpse into my life, this 3D universe was an absolute blast for me to build!</p>
-        <a href="http://www.roscioli.dev" target="_blank" rel="noreferrer"><img src="https://i.imgur.com/in2rddO.jpg" alt="3d world" /></a>
+		<h2 className='projects-title'>My THREE.js Universe</h2>
+        <p>An amusing and intereactive way to get a glimpse into my life, this 3D universe was an absolute blast for me to build! Check out the <a className="projects-links" href="https://github.com/TonyRosh/tonys-universe">GitHub Repo Here!</a> </p>
+        <a href="https://github.com/TonyRosh/tonys-universe" target="_blank" rel="noreferrer"><img src="https://i.imgur.com/in2rddO.jpg" alt="3d world" /></a>
 	</section>
 	<section>
 		<h2 className='projects-title'>Sky Bear Brewery</h2>
-        <p>The homepage for Sky Bear Brewery in Loveland Colorado! LINK COMING SOON!</p>
-        <img src="https://i.imgur.com/puRZH9l.png" alt="3d world" />
+        <p>The homepage for Sky Bear Brewery in Loveland Colorado! Check out the <a className="projects-links" href="https://github.com/TonyRosh/brewery-frontend">Front End Here</a> and the <a className="projects-links" href="https://github.com/TonyRosh/brewery_backend">Back End Here,</a> or click the image for a YouTube demo!</p>
+        <a href="https://www.youtube.com/watch?v=aIBO73XwNDA&t=73s" target="_blank" rel="noreferrer"><img src="https://i.imgur.com/puRZH9l.png" alt="Sky Bear Brewery" /></a>
 	</section>
 	<section>
 		<h2 className='projects-title'>Snack Pak Records</h2>
-        <p>A landing page for a mock record label! LINK COMING SOON!</p>
-        <img src="https://i.imgur.com/cTe7HEV.jpg" alt="3d world" />
+        <p>A landing page for a mock record label! Check out the <a className="projects-links" href="https://github.com/TonyRosh/snack-pak-frontend">Front End Here</a> and the <a className="projects-links" href="https://github.com/TonyRosh/snack-pak-backend">Back End Here,</a> or click the image for a YouTube demo!</p>
+        <a href="https://www.youtube.com/watch?v=pJ9tbZiHnVQ&t=15s" target="_blank" rel="noreferrer"><img src="https://i.imgur.com/cTe7HEV.jpg" alt="Snack Pak Records" /></a>
 	</section>
 	<section>
 		<h2 className='projects-title'>Gym App</h2>
-        <p>My first partnered project from my time at Flatiron School! LINK COMING SOON!</p>
-        <img src="https://i.imgur.com/5J34s2N.jpg" alt="3d world" />
+        <p>My first partnered project from my time at Flatiron School! Check out the <a className="projects-links" href="https://github.com/TonyRosh/gym-app">GitHub Repo Here,</a> or click the image for a YouTube demo!</p>
+        <a href="https://www.youtube.com/watch?v=3QUCyPf4BEI" target="_blank" rel="noreferrer"><img src="https://i.imgur.com/5J34s2N.jpg" alt="Gym App" /></a>
 	</section>
 </div>
 <div>
